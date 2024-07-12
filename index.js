9cbd94d30d697cfafba4c0d3bc73891a793d5923
chamod kavinda <kavindachamod100@gmail.com>
2024-07-12 22:05:30 +0530

    implement get data function

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-
+const methodOverride = require('method-override');
+const Campground = require('./models/campground');
+const ejsMate = require('ejs-mate');
 mongoose.connect('mongodb://localhost:27017/camp-haven');
 
 const db = mongoose.connection;
@@ -14,9 +16,36 @@ db.once("open",()=>{
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
+app.engine('ejs',ejsMate);
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
 app.get('/',(req,res)=>{
     res.render('home');
 })
+
+app.get('/campgrounds',async (req,res)=>{
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index',{campgrounds});
+})
+
+
+
+app.post('/campgrounds', async (req,res) =>{
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/:id',async (req,res)=>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show',{campground});
+})
+
+
+
+
+
 
 app.listen(8080,(req,res)=>{
     console.log('server running');
