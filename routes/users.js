4f09219605fdd2c786d08241de16683fc00c5d55
chamod kavinda <kavindachamod100@gmail.com>
2024-07-20 22:29:12 +0530

    login middleware

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
+const { storeReturnTo } = require('../middleware');
 
 router.get('/register', (req,res) =>{
     res.render('users/register');
@@ -25,9 +26,23 @@ router.get('/login', (req,res) =>{
     res.render('users/login');
 })
 
-router.post('/login',passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req,res) =>{
-    req.flash('success', 'welcome back');
-    res.redirect('/campgrounds');
-})
+router.post('/login',
+    storeReturnTo,
+    passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}),
+    (req, res) => {
+        req.flash('success', 'Welcome back!');
+        const redirectUrl = res.locals.returnTo || '/campgrounds';
+        res.redirect(redirectUrl);
+    });
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+}); 
 
 module.exports = router;
